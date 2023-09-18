d60667f9739eef3891bfe62d1ff28bef130c1a0e
Janyl Jumadinova <jjumadinova@allegheny.edu>
2023-09-18 09:51:55 -0400

    Add class activity from September 18th

--- PowerOperations/src/main.py
+++ PowerOperations/src/main.py
@@ -16,22 +16,29 @@ Expression course.
 import math
 import suffix
 
-# TODO: Write function to calculate result of various numbers 
+# Write function to calculate result of various numbers 
 #       raised to various powers.
+def raise_to_power(number, power):
+  """ Calculates result of a number raised to a power """
+  sq_root = math.pow(number, power)
+  return sq_root
 
 # TODO: Write function to calculates nth root of a number 
 #       by raising to fractional power
 
-# TODO: Print program introduction
+# Print program introduction
 print("Power and root calculator", end = "\n\n")
 
-# TODO: Collect user information
+# Collect user information
+user_num = int(input("Enter a number: "))
+user_pow = int(input("Enter a power: "))
 
-# TODO: Perform calculations using functions
+# Perform calculations using functions
+raised = raise_to_power(user_num, user_pow)
 
-# TODO: Use suffix to get the proper number suffix: 1st, 2nd...
+# Use suffix to get the proper number suffix: 1st, 2nd...
 num_suffix = suffix.determine(user_pow)
 
-# TODO: Print results for both computations using same power
+# Print results for both computations using same power
 print(f"{user_num} raised to the {user_pow}{num_suffix} power: {raised}")
 print(f"{user_num} taken to the {user_pow}{num_suffix} root: {rooted}")
