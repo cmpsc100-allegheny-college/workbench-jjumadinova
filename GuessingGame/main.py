403dbde013a65d3a4e44702bc60aa60da0cfa9bc
Janyl Jumadinova <jjumadinova@allegheny.edu>
2023-10-04 10:18:02 -0400

    Add solution from class on 10/04/23

--- GuessingGame/main.py
+++ GuessingGame/main.py
@@ -8,20 +8,41 @@ def below_number(guess: int = 0, number: int = 0) -> bool:
     """Compares guess to number to check if it is lower"""
     return guess < number
 
+def outside_range(guess: int = 0, low: int = 0, high: int = 1) -> bool:
+    return guess > high or guess < low
+
 def main():
     lower_limit = 1
     upper_limit = 10
     number_to_guess = random.randint(lower_limit, upper_limit)
-    # print(number_to_guess)
-    guess = int(input(f"Guess a number between {lower_limit} and {upper_limit}: "))
+    print(number_to_guess)
+    
+    guess = input(f"Guess a number between {lower_limit} and {upper_limit}: ")
+    
+    try:
+        guess = int(guess)
+    except ValueError:
+        print("Your number needs to be an integer.")
 
-    while(guess != number_to_guess):
-        guess = int(input("Wrong guess. Guess again?"))
-        if above_number(guess, number_to_guess):
-            print("Your number is too high")
+    if(isinstance(guess, int)):
+        guess = int (guess)
+
+    while (guess != number_to_guess):
+
+        try:
+            guess = int(input("Wrong guess. Guess again? "))
+            # print(isinstance(guess, int))
+        except ValueError:
+            print("Your number needs to be an integer.")
+        
+        if outside_range(guess, lower_limit, upper_limit):
+            print("Your number is outside the range. ")
+            # pass
+        elif above_number(guess, number_to_guess):
+            print("Your number is too high. ")
         elif below_number(guess, number_to_guess):
-            print("Your number is too low")
-
+            print("Your number is too low.")
+        
     print("You won!")
 
 if __name__ == "__main__":
