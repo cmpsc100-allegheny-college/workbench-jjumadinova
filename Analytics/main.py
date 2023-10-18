d968a80b824c94b7bfa0c9acda9d8374a858526d
Janyl Jumadinova <jjumadinova@allegheny.edu>
2023-10-18 09:24:58 -0400

    Class program from 10/18

---
+++ Analytics/main.py
@@ -0,0 +1,17 @@
+fruit = ["apple", "banana", "orange", "apple", "strawberry"]
+
+print(fruit)
+
+# add an item to the list
+fruit.append("pear")
+
+print(fruit)
+
+# get an index/position of an item in the list
+print("index of apple: ", fruit.index("apple"))
+
+# get a size of the list
+print("number of elements in fruit: ", len(fruit))
+
+# get the number of times an item appear in the list
+print("count of apple: ", fruit.count("apple"))
