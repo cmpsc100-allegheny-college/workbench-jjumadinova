e506104c2b213a3762eb044821443375cb789c3b
Janyl Jumadinova <jjumadinova@allegheny.edu>
2023-11-03 14:20:17 -0400

    Add activity of 11/3

--- Analytics/termworld_analytics.py
+++ Analytics/termworld_analytics.py
@@ -5,23 +5,30 @@ def load_file(filename: str = "") -> dict:
     data = json.load(file)
 
     for item in data:
-        print("Item: ", item)
+        #print("Item: ", item)
         idx = data.index(item)
-        print("idx: ", idx)
+        #print("idx: ", idx)
         id = {"#": idx + 1} # dictionary
-        print("id: ", id)
-        print("old data[idx]: ", data[idx])
+        #print("id: ", id)
+        #print("old data[idx]: ", data[idx])
         # merge dictionaries
         id.update(data[idx])
-        print("new id dic: ", id)
+        #print("new id dic: ", id)
         data[idx] = id
-        print("new data[idx]: ", data[idx])
+        #print("new data[idx]: ", data[idx])
 
     return data
 
 def main():
     global DATA
     DATA = load_file(filename = "data/data.json")
+
+    rows = []
+
+    for row in DATA:
+        if int(row["Logins"]) >= 40:
+            rows.append(row)
+    print(rows)
 
 if __name__ == "__main__":
     main()
